fb3d9669c8816ec5768f9940384c170cd64ca4cc
OisCircle <793414670@qq.com>
2018-05-02 17:14:11 +0800

    nnn

--- src/main/java/com/qcq/spring_boot_demo/bean/Cat.java
+++ src/main/java/com/qcq/spring_boot_demo/bean/Cat.java
@@ -2,21 +2,31 @@ package com.qcq.spring_boot_demo.bean;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 public class Cat {
-	@NotNull
+	@NotEmpty(message = "名字不能为空")
 	private String name;
+	@NotNull(message = "年龄不能为空")
 	@Max(value = 10,message = "年龄最大不能高于10")
 	@Min(value = 0,message = "年龄最小不能低于0")
 	private int age;
 
+	public Cat() {
+
+	}
 	@Override
 	public String toString() {
 		return "Cat{" +
 				"name='" + name + '\'' +
 				", age=" + age +
 				'}';
+	}
+
+	public Cat(@NotEmpty(message = "名字不能为空") String name, @NotNull(message = "年龄不能为空") @Max(value = 10, message = "年龄最大不能高于10") @Min(value = 0, message = "年龄最小不能低于0") int age) {
+		this.name = name;
+		this.age = age;
 	}
 
 	public String getName() {
---
+++ src/test/java/com/qcq/spring_boot_demo/MybatisPlusTest.java
@@ -0,0 +1,48 @@
+package com.qcq.spring_boot_demo;
+
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.qcq.spring_boot_demo.entity.Bird;
+import com.qcq.spring_boot_demo.entity.User;
+import com.qcq.spring_boot_demo.mapper.BirdMapper;
+import com.qcq.spring_boot_demo.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class MybatisPlusTest {
+	@Autowired
+	BirdMapper birdMapper;
+	@Autowired
+	UserService userService;
+	@Test
+	public void wrapper() {
+		List<Bird> birdList = birdMapper.selectPage(new Page<Bird>(1, 2), new EntityWrapper<>());
+		System.out.println(birdList);
+		Page<Bird> birdPage=new Page<>();
+
+		birdPage.setRecords(birdList);
+		System.out.println(birdPage);
+ 	}
+
+	@Test
+	public void warpper2() {
+		EntityWrapper<User> ew = new EntityWrapper<User>();
+		ew.where("user_name={0}", "'zhangsan'").and("id=1")
+				.orNew("user_status={0}", "0").or("status=1")
+				.notLike("user_nickname", "notvalue")
+				.andNew("new=xx").like("hhh", "ddd")
+				.andNew("pwd=11").isNotNull("n1,n2").isNull("n3")
+				.groupBy("x1").groupBy("x2,x3")
+				.having("x1=11").having("x3=433")
+				.orderBy("dd").orderBy("d1,d2");
+		System.out.println(ew.getSqlSegment());
+
+	}
+}
